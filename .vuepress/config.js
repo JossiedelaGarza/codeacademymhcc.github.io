77ba58593f219a17b380bce38b0b2df5f6720c88
George Mandis <george@snaptortoise.com>
2018-04-25 19:22:21 -0700

    using vuepress

---
+++ .vuepress/config.js
@@ -0,0 +1,49 @@
+// .vuepress/config.js
+module.exports = {
+  title: "MHCC Code Academy 2018",
+  serviceWorker: true,  
+  themeConfig: {  
+    repo: 'CodeAcademyMHCC/codeacademymhcc.github.io',
+    editLinks: true,      
+    nav: [
+        
+        // { text: 'Resources', link: '/resources/' },
+        { text: 'Treehouse', link: 'http://teamtreehouse.com/' },
+        { text: 'Slack', link: 'https://mhcccodeacademy2018.slack.com/' },        
+      ],
+      sidebar: [
+        {
+          title: 'Syllabus',
+          collapsable: true,
+          children: [
+            ['syllabus/week1', "Week 1"],
+            // ['#', "Week 2"],
+            // ['#', "Week 3"],
+            // ['#', "Week 4"],
+            // ['#', "Week 5"],
+            // ['#', "Week 6"],
+            // ['#', "Week 7"],
+            // ['#', "Week 8"],
+            // ['#', "Week 9"],
+            // ['#', "Week 10"],
+          ]
+        },
+        {
+            title: 'Services & Resources',
+            collapsable: true,
+            children: [
+              ['syllabus/week1', "Week 1"],
+              // ['#', "Week 2"],
+              // ['#', "Week 3"],
+              // ['#', "Week 4"],
+              // ['#', "Week 5"],
+              // ['#', "Week 6"],
+              // ['#', "Week 7"],
+              // ['#', "Week 8"],
+              // ['#', "Week 9"],
+              // ['#', "Week 10"],
+            ]
+          },        
+      ]
+    }
+}
---
+++ README.md
@@ -0,0 +1,16 @@
+---
+title: MHCC Code Academy
+home: true
+heroText: Code Academy 2018
+heroImage: /images/mhcc-logo.jpg
+description: An introduction to  web development
+# actionText: Get Started →
+# actionLink: /
+# features:
+# - title: Simplicity First
+#   details: Minimal setup with markdown-centered project structure helps you focus on writing.
+# - title: Vue-Powered
+#   details: Enjoy the dev experience of Vue + webpack, use Vue components in markdown, and develop custom themes with Vue.
+# - title: Performant
+#   details: VuePress generates pre-rendered static HTML for each page, and runs as an SPA once a page is loaded.
+---
